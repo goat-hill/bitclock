81f70b0b6998c781e5ab494ecf626166bc30dac2
Brady Law <bradyl16@gmail.com>
2024-06-13 19:07:38 -0700

    modify copy

--- bitclock-web/src/app/page.tsx
+++ bitclock-web/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             <Button component={Link} href="https://github.com/goat-hill/bitclock" target="_blank" leftSection={<IconBrandGithub size={14} />}>Code on GitHub</Button>
             <Button component={Link} href="/connect" variant="gradient" gradient={{ from: "indigo", to: "cyan", deg: 217 }} leftSection={<IconBluetooth size={14} />}>Connect to device</Button>
           </Flex>
-          <Text ta="center">Shipping <strong>July 2024</strong>. Join mailing list to stay updated!</Text>
+          <Text ta="center">Shipping <strong>July 2024</strong>. Complete the form to be notified when it's available!</Text>
           <Box w={{ base: '100%', xs: 480, md: 600 }}>
           <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd2UAob-NJfPU_3VcjZiAaslMEGd6qegJ8XPgLOtxQYYuO0Aw/viewform?embedded=true"
             width="100%" height="1500" frameBorder={0} marginHeight={0} marginWidth={0}>Loading…</iframe>
