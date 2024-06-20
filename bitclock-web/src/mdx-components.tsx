c752d51d76bfe33c1844b0d52e689a1a572b2771
Brady Law <bradyl16@gmail.com>
2024-06-20 15:01:34 -0700

    progress

--- bitclock-web/src/mdx-components.tsx
+++ bitclock-web/src/mdx-components.tsx
@@ -3,11 +3,42 @@ import "@mantine/code-highlight/styles.css";
 
 import type { MDXComponents } from "mdx/types";
 import { CodeHighlight } from "@mantine/code-highlight";
-// This file is required to use MDX in `app` directory.
+import { Anchor, Title } from "@mantine/core";
+import Link from "next/link";
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    // Allows customizing built-in components, e.g. to add styling.
-    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    h1: (props) => <Title order={1}>{props.children}</Title>,
+    h2: (props) => (
+      <Title order={2} mt="xl" mb="sm">
+        {props.children}
+      </Title>
+    ),
+    h3: (props) => (
+      <Title order={3} mt="xl" mb="sm">
+        {props.children}
+      </Title>
+    ),
+    h4: (props) => (
+      <Title order={4} mt="xl" mb="sm">
+        {props.children}
+      </Title>
+    ),
+    h5: (props) => (
+      <Title order={5} mt="xl" mb="sm">
+        {props.children}
+      </Title>
+    ),
+    h6: (props) => (
+      <Title order={6} mt="xl" mb="sm">
+        {props.children}
+      </Title>
+    ),
+    a: (props) => (
+      <Anchor component={Link} target="_blank" href={props.href as string}>
+        {props.children}
+      </Anchor>
+    ),
     code: (props) => {
       const lang = props.className?.replace("language-", "");
       return <CodeHighlight language={lang} code={props.children as string} />;
