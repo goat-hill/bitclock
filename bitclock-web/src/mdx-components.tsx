f11f509234b0936425aff72958b8acc7efb6cf07
Brady Law <bradyl16@gmail.com>
2024-08-07 16:19:50 -0700

    Update mdx-components.tsx

--- bitclock-web/src/mdx-components.tsx
+++ bitclock-web/src/mdx-components.tsx
@@ -3,34 +3,34 @@ import "@mantine/code-highlight/styles.css";
 
 import type { MDXComponents } from "mdx/types";
 import { CodeHighlight } from "@mantine/code-highlight";
-import { Anchor, Title, Code } from "@mantine/core";
+import { Anchor, Title, Code, rem } from "@mantine/core";
 import Link from "next/link";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: (props) => <Title order={1}>{props.children}</Title>,
     h2: (props) => (
-      <Title order={2} mt="xl" mb="sm">
+      <Title order={2} mt={rem(55)}>
         {props.children}
       </Title>
     ),
     h3: (props) => (
-      <Title order={3} mt="xl" mb="sm">
+      <Title order={3} mt={rem(45)}>
         {props.children}
       </Title>
     ),
     h4: (props) => (
-      <Title order={4} mt="xl" mb="sm">
+      <Title order={4} mt={rem(35)}>
         {props.children}
       </Title>
     ),
     h5: (props) => (
-      <Title order={5} mt="xl" mb="sm">
+      <Title order={5} mt={rem(25)}>
         {props.children}
       </Title>
     ),
     h6: (props) => (
-      <Title order={6} mt="xl" mb="sm">
+      <Title order={6} mt={10}>
         {props.children}
       </Title>
     ),
