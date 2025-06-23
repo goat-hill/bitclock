331b4c104285c7c6263d7acd91c95b0c06e044f8
Brady Law <bradyl16@gmail.com>
2025-06-23 09:09:55 -0700

    typescript hack

--- bitclock-web/src/libs/util.ts
+++ bitclock-web/src/libs/util.ts
@@ -1,6 +1,6 @@
 function stringToArrayBuffer(str: string): ArrayBuffer {
   const encoder = new TextEncoder(); // TextEncoder defaults to UTF-8
-  return encoder.encode(str).buffer;
+  return encoder.encode(str).buffer as ArrayBuffer;
 }
 
 export { stringToArrayBuffer };
